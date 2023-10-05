f388f8edab4cb68864493377c804b419a4f09508
thebowenfeng <857514.leofeng@gmail.com>
2023-10-06 01:30:52 +1100

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import firebaseSdkConfig from "./firebaseConfig.json"
+import { initializeApp } from "firebase/app"
+import { getAnalytics } from "firebase/analytics"
+
+export function App() {
+  const config = firebaseSdkConfig.result.sdkConfig
+  const app = initializeApp(config)
+  const analytics = getAnalytics(app)
+
+  console.log(app)
+
+  return <h1>{process.env.NODE_ENV} abcdef</h1>;
+}
