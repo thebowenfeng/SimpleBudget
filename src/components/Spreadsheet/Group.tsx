7283dbecd0037cf6c0c999d327052fbff5c7e852
thebowenfeng <857514.leofeng@gmail.com>
2023-10-10 18:32:09 +1100

    added dragging

--- src/components/Spreadsheet/Group.tsx
+++ src/components/Spreadsheet/Group.tsx
@@ -1,6 +1,16 @@
 import {styled} from 'styled-components'
 import { Button, Heading, IconButton } from '@chakra-ui/react'
-import { Children, ReactNode, useMemo, useRef, useState } from 'react'
+import {
+  Children,
+  Dispatch,
+  ForwardedRef,
+  forwardRef,
+  ReactNode,
+  SetStateAction, useEffect,
+  useMemo,
+  useRef,
+  useState
+} from 'react'
 import { ArrowDownIcon, ArrowForwardIcon } from '@chakra-ui/icons'
 import { isMobile } from 'react-device-detect'
 
@@ -8,6 +18,7 @@ interface Props {
   title: string,
   assigned: number,
   available: number,
+  displayChild: boolean,
   children: ReactNode
 }
 
@@ -18,7 +29,7 @@ const RootContainer = styled.div`
   min-height: 40px;
   width: 100%;
   align-items: center;
-  background-color: rgba(215, 215, 215, 0.53);
+  background-color: rgba(215, 215, 215, 1);
   padding-right: 20px;
   padding-left: 20px;
 `
@@ -36,12 +47,12 @@ const LabelContainer = styled.div`
 const ChildrenContainer = styled.div`
   display: flex;
   flex-direction: column;
-  transition: max-height 1s linear;
+  transition: max-height 1s linear, min-height 1s linear;
+  overflow: hidden;
 `
 
-export default function Group(props: Props) {
+const Group = forwardRef(function GroupFn(props: Props, ref: ForwardedRef<HTMLDivElement>) {
   const containerRef = useRef<HTMLDivElement>();
-  const [isFolded, setIsFolded] = useState<boolean>(false);
   const maxChildrenHeight = useMemo<number>(() => {
     let height = 0;
     for (const child of Children.toArray(props.children)) {
@@ -49,29 +60,43 @@ export default function Group(props: Props) {
     }
     return 0;
   }, [props.children])
+  const [isFolded, setIsFolded] = useState<boolean>(false);
 
   const fold = () => {
-    switch (containerRef.current.style.maxHeight) {
-      case '0px':
-        containerRef.current.style.maxHeight = '1500px';
-        containerRef.current.style.visibility = 'visible'
-        setIsFolded(false);
-        break;
-      case '1500px':
-        containerRef.current.style.maxHeight = '0';
-        setTimeout(() => {
-          containerRef.current.style.visibility = 'hidden'
-        }, 1000)
-        setIsFolded(true);
-        break;
-    }
+    setIsFolded((prev) => {
+      if (props.displayChild) {
+        switch (prev) {
+          case true:
+            containerRef.current.style.minHeight = '1000px';
+            containerRef.current.style.maxHeight = '1000px';
+            return false;
+          case false:
+            containerRef.current.style.minHeight = '0px';
+            containerRef.current.style.maxHeight = '0px';
+            return true;
+        }
+      }
+      return !prev;
+    })
   }
+
+  useEffect(() => {
+    if (props.displayChild) {
+      if (isFolded) {
+        containerRef.current.style.minHeight = '0px';
+        containerRef.current.style.maxHeight = '0px';
+      } else {
+        containerRef.current.style.minHeight = '1000px';
+        containerRef.current.style.maxHeight = '1000px';
+      }
+    }
+  }, [props.displayChild])
 
   return(
     <>
-      <RootContainer>
-        <IconButton aria-label={'fold'} icon={isFolded ? <ArrowForwardIcon /> : <ArrowDownIcon />}
-                    onClick={fold} variant={'link'} fontSize={isMobile ? '30px' : null}>Fold</IconButton>
+      <RootContainer ref={ref}>
+        {props.displayChild && <IconButton aria-label={'fold'} icon={isFolded ? <ArrowForwardIcon /> : <ArrowDownIcon />}
+                    onClick={fold} variant={'link'} fontSize={isMobile ? '30px' : null}>Fold</IconButton>}
         <Heading fontSize={isMobile ? '30px' : '20px'}>{props.title}</Heading>
         <div style={{display: 'flex', flexDirection: 'row', marginLeft: 'auto'}}>
           <LabelContainer>
@@ -82,9 +107,11 @@ export default function Group(props: Props) {
           </LabelContainer>
         </div>
       </RootContainer>
-      <ChildrenContainer ref={containerRef} style={{maxHeight: '1500px', visibility: 'visible'}}>
+      {props.displayChild && <ChildrenContainer ref={containerRef} style={{minHeight: '1000px', maxHeight: '1000px'}}>
         {props.children}
-      </ChildrenContainer>
+      </ChildrenContainer>}
     </>
   )
-}
+})
+
+export default Group;
--- src/components/Spreadsheet/SpreadsheetView.tsx
+++ src/components/Spreadsheet/SpreadsheetView.tsx
@@ -2,6 +2,17 @@ import styled from 'styled-components'
 import { Button } from '@chakra-ui/react'
 import Group from './Group'
 import { isMobile } from 'react-device-detect'
+import { useCallback, useState, MouseEvent, useRef, MutableRefObject, useEffect } from 'react'
+
+interface DragState {
+  isMouseDown: boolean;
+  draggedItem: TempTest;
+}
+
+interface TempTest {
+  title: string,
+  ref: MutableRefObject<HTMLDivElement> | null,
+}
 
 const ActionsBar = styled.div`
   height: ${isMobile ? '100px' : '50px'};
@@ -16,7 +27,7 @@ const ActionsBar = styled.div`
 const ViewContainer = styled.div`
   display: flex;
   flex-direction: column;
-  overflow-y: scroll;
+  overflow: scroll;
   max-height: calc(100vh - 150px);
   padding-bottom: 30px;
   scrollbar-gutter: stable;
@@ -47,45 +58,115 @@ const LabelContainer = styled.div`
   height: 100%;
 `
 
+function genTempTest(): TempTest[] {
+  const a = [];
+  for (let i = 0; i < 3; i++){
+    const ref = useRef<HTMLDivElement>()
+    a.push({title: i.toString(), ref: ref})
+  }
+  return a;
+}
+
+function getIndex(title: string, lst: TempTest[]): number {
+  for (let i = 0; i < lst.length; i++) {
+    const item = lst[i];
+    if (item.title == title) {
+      return i;
+    }
+  }
+  return 9999;
+}
+
 export default function SpreadsheetView() {
+  const dragState = useRef<DragState>({ isMouseDown: false, draggedItem: null });
+  const groups = useRef<TempTest[]>(genTempTest());
+  const [signal, setSignal] = useState<boolean>(false);
+  const testRef = useRef<HTMLDivElement>()
+  const [displayChild, setDisplayChild] = useState<boolean>(true);
+
+  const onMouseUp = useCallback(() => {
+    setDisplayChild(true);
+    dragState.current.isMouseDown = false;
+    if (dragState.current.draggedItem) {
+      dragState.current.draggedItem.ref.current.style.border = null;
+      dragState.current.draggedItem = null;
+    }
+  }, [])
+
+  const onMouseDown = useCallback(() => {
+    dragState.current.isMouseDown = true;
+  }, [])
+
+  const onMouseMoveFn = useCallback(() => {
+    // document.getElementById("abcdefg").innerHTML = (event.pageY - (testRef.current.offsetTop - testRef.current.scrollTop)).toString()
+    if (dragState.current.isMouseDown) {
+      setDisplayChild(false);
+      if (!dragState.current.draggedItem) {
+        for (const group of groups.current) {
+          if (group.ref.current.matches(":hover")) {
+            dragState.current.draggedItem = group;
+            break;
+          }
+        }
+      }
+
+      if (dragState.current.draggedItem) {
+        dragState.current.draggedItem.ref.current.style.border = "2px solid blue";
+
+        for (const group of groups.current) {
+          if (group.ref.current.matches(":hover") && group.title != dragState.current.draggedItem.title) {
+            const currInd = getIndex(dragState.current.draggedItem.title, groups.current)
+            const newInd = getIndex(group.title, groups.current)
+
+            groups.current[newInd] = dragState.current.draggedItem;
+            groups.current[currInd] = group
+            setSignal((prevState) => !prevState);
+            break;
+          }
+        }
+
+        for (const group of groups.current) {
+          if (group.title != dragState.current.draggedItem.title) {
+            group.ref.current.style.border = null;
+          }
+        }
+      }
+    }
+  }, [])
+
+  useEffect(() => {
+    document.addEventListener("mousedown", onMouseDown);
+    document.addEventListener("mouseup", onMouseUp);
+
+    return () => {
+      document.removeEventListener("mousedown", onMouseDown);
+      document.removeEventListener("mouseup", onMouseUp);
+    }
+  }, [])
+
   return (
-    <>
+    <div>
       <ActionsBar>
         <Button variant={'link'} fontSize={isMobile ? '2.5rem' : null}>Create group</Button>
         <Button sx={{marginLeft: 'auto'}} variant={'link'} fontSize={isMobile ? '2.5rem' : null}>Delete group</Button>
       </ActionsBar>
       <LabelHeader>
         <LabelContainer>
-          <h1 style={{fontSize: isMobile ? '1.75rem' : null}}>Available</h1>
+          <h1 style={{fontSize: isMobile ? '1.75rem' : null}} id={"abcdefg"}>Available</h1>
         </LabelContainer>
         <LabelContainer>
-          <h1 style={{fontSize: isMobile ? '1.75rem' : null}}>Assigned</h1>
+          <h1 style={{fontSize: isMobile ? '1.75rem' : null}} id={"hijklmn"}>Assigned</h1>
         </LabelContainer>
       </LabelHeader>
-      <ViewContainer>
-        <Group title={"MyGroup1"} assigned={100} available={80}>
-          <h1 style={{fontSize: '5rem'}}>Test</h1>
-          <h1 style={{fontSize: '5rem'}}>Test</h1>
-          <h1 style={{fontSize: '5rem'}}>Test</h1>
-          <h1 style={{fontSize: '5rem'}}>Test</h1>
-          <h1 style={{fontSize: '5rem'}}>Test</h1>
-          <h1 style={{fontSize: '5rem'}}>Test</h1>
-          <h1 style={{fontSize: '5rem'}}>Test</h1>
-          <h1 style={{fontSize: '5rem'}}>Test</h1>
-          <h1 style={{fontSize: '5rem'}}>Test</h1>
-        </Group>
-        <Group title={"MyGroup1"} assigned={100} available={80}>
-          <h1 style={{fontSize: '5rem'}}>Test</h1>
-          <h1 style={{fontSize: '5rem'}}>Test</h1>
-          <h1 style={{fontSize: '5rem'}}>Test</h1>
-          <h1 style={{fontSize: '5rem'}}>Test</h1>
-          <h1 style={{fontSize: '5rem'}}>Test</h1>
-          <h1 style={{fontSize: '5rem'}}>Test</h1>
-          <h1 style={{fontSize: '5rem'}}>Test</h1>
-          <h1 style={{fontSize: '5rem'}}>Test</h1>
-          <h1 style={{fontSize: '5rem'}}>Test</h1>
-        </Group>
+      <ViewContainer onMouseMove={onMouseMoveFn} ref={testRef}>
+        {groups.current.map((obj: TempTest) => {
+          return (
+            <Group title={obj.title} assigned={100} available={80} ref={obj.ref} displayChild={displayChild}>
+
+            </Group>
+          )
+        })}
       </ViewContainer>
-    </>
+    </div>
   )
 }
