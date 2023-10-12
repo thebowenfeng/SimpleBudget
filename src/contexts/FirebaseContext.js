b81cdfe98843c232946f8b332ce34a2a3542b73d
thebowenfeng <857514.leofeng@gmail.com>
2023-10-12 17:48:53 +1100

    updated gitignore, removed js and map files

--- src/contexts/FirebaseContext.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.FirebaseContext = void 0;
-const react_1 = require("react");
-exports.FirebaseContext = (0, react_1.createContext)(undefined);
-//# sourceMappingURL=FirebaseContext.js.map
